7016a4dc610beb2a3b55f2ba358745603cc3dbe2
Gan-Xing <599153574@qq.com>
2023-11-29 16:52:32 +0800

    build: add GXVideo

--- docs/assets/navigation.js
+++ docs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslLyrXTOSSwuVtJRKkgsyQAKJIO4qcX6UAm9jJLcHKBsdmZeipKVoZFFbSwAMUbVPD0AAAA="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJyjwjLTEnNV9JRKkgsyQAKJOckFhenFutDJfQySnJzgLLZmXkpSlaGRha1OnC9vpXOIMWYeqES+PS6R4R6IjSmleYll2Tm54GsDfVE1WdmUhsLAPzNpkCwAAAA"
